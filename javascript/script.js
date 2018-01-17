baecc23c8486b77cb69c93c31f7cc28ac95b38ad
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2018-01-17 11:03:44 +0100

    add some text to leader board and link leader-board.html with menu-html

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@
   function Redirect() {
     window.location="menu.html";
   }
+//redriects from button 'leaderboard' on menu.html to leader-board.html
+  function lBoardRedirect() {
+    window.location="leader-board.html";
+  }
 //For leaderboard drop down
   $('.show-info').click( function() {
 	$(this).find('.more-info').slideToggle('slow');
