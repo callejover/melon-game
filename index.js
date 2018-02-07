c16732dfc79c99a4ef0760ec6bb16fa67c2d4ea0
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2018-02-07 15:38:32 +0100

    add login to data base start kit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express=require("express");
+var bodyParser=require('body-parser');
+
+var app = express();
+
+var authenticateController=require('./controllers/authenticate-controller');
+var registerController=require('./controllers/register-controller');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+/* route to handle login and registration */
+app.post('/api/register',registerController.register);
+app.post('/api/authenticate',authenticateController.authenticate);
+app.listen(8012);
