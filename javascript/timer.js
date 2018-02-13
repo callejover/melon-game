56a0492f8011f4f267dab5a556a07d47efae151b
Carl Jover <carljover@Carls-MacBook-Pro.local>
2018-02-13 12:09:46 +0100

    Counts down and starts the game

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,7 +1,25 @@
 let timer = 60;
+let startGameTimer = 5;
 
 let interval = setInterval(function() {
     timer--;
     $('.timer').text(timer);
     if (timer === 0) clearInterval(interval);
+}, 1000);
+
+let startGameInterval = setInterval(function() {
+    startGameTimer--;
+    $('#start-game-timer').text(startGameTimer);
+    if (startGameTimer === 0) {
+        clearInterval(startGameInterval);
+        $('#start-game-timer').html('START!!!');
+        setTimeout(function() { 
+            $(".close").click(); 
+        }, 1000);
+        let interval = setInterval(function() {
+            timer--;
+            $('.timer').text(timer);
+            if (timer === 0) clearInterval(interval);
+        }, 1000);
+    }
 }, 1000);
