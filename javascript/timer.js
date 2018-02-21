1dc8cdc2717b4c672d2fd705e9369954bf6dea91
Jeowi <32056595+Jeowi@users.noreply.github.com>
2018-02-21 10:29:09 +0100

    created end game modal with redirect to previous page (menu page)

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,14 +1,21 @@
 
-let startTimer = "10:06"; //start tid, startar tiden samtidigt som modalen räknar ner till "Start!!!"
+let startTimer = "00:10"; //start tid, startar tiden samtidigt som modalen räknar ner till "Start!!!"
   let interval = setInterval(function() { //Räknaren under spelet
     let timer = startTimer.split(':'); //Separerar sekunder från minuter
-    let minutes = parseInt(timer[0], 10); //Konverterar stringen till ett int
-    let seconds = parseInt(timer[1], 10);//Konverterar stringen till ett int
+    let minutes = parseInt(timer[0], 10); //Konverterar stringen till en int
+    let seconds = parseInt(timer[1], 10);//Konverterar stringen till en int
         --seconds; //Räkna ned 1 i taget
     minutes = (seconds < 0) ? --minutes : minutes; //?? Vet ej ?? Om man ej har den så börjar den på 10:00 och efter en sekund blir den 10:59
-        if (minutes < 0) clearInterval(interval);
+        if (minutes < 0) {
+            clearInterval(interval)
+        };
             seconds = (seconds < 0) ? 59 : seconds; // Om minutes är mindre än 0, displaya 59 och räkna ned seconds
             seconds = (seconds < 10) ? '0' + seconds : seconds; // Om seconds är mindre än 10 displaya 0 + seconds + seconds (00:09)
+            if (minutes == 0 && seconds == 0) {
+                clearInterval(interval)
+                $('#end-game').modal('show', 'display', 'block')
+                $('#end-game-modal').css('display', 'block')                
+            }
         $('.timer').html(minutes + ':' + seconds); //Skjut in nedräknare in i HTML:en
   startTimer = minutes + ':' + seconds; //startTimer representerar minutes och seconds (10:06)
 }, 1000); //räkna ned en sekund i taget
@@ -26,3 +33,7 @@ let startGameInterval = setInterval(function() { // Räknaren man möts av när 
     }
 }, 1000);
 
+// SIMPLE GOBACK FUNCTION -redirects you to last page
+function goBack() {
+    window.history.back();
+  }
