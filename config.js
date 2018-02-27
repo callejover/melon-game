cb5df4921afd379c7eeb4f3e50a281ad7e5ba999
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2018-02-27 22:03:46 +0100

    change name on db again

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'api-tester'
+  database : 'api-test'
 });
 connection.connect(function(err){
 if(!err) {
