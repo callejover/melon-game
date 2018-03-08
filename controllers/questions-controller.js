2fbfea4172dff1335f786546009ac4ac7229f991
Carl Jover <carljover@Carls-MacBook-Pro.local>
2018-03-08 22:55:28 +0700

    cleaning code

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -4,7 +4,6 @@ module.exports.question = function(req,res) {
 
     connection.query('SELECT * FROM questions', function (error, results, fields) {
       if (error) {
-        console.log('hej');
           res.json({
             status:false,
             message:'there are some error with query'
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,20 @@
 var connection = require('./../config');
-module.exports.user=function(req,res){
-var email = req.query.email;
+module.exports.user = function(req,res){
+    var email = req.query.email;
 
-connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-    if (error) {
+    connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+        if (error) {
+            res.json({
+            status:false,
+            message:'there are some error with query'
+            })
+        } else {
         res.json({
-        status:false,
-        message:'there are some error with query'
+            status: true,
+            data: results[0]
         })
-    } else {
-    console.log(results);
-    res.json({
-        status: true,
-        data: results[0]
-    })
-    }
-});
+        }
+    });
 
-return;
-}
+    return;
+    }
